98e219ea53248949693f994c00535145cd6b02ed
JetMuffin <564936642@qq.com>
2014-07-19 19:42:28 +0800

    增加分享图片

--- shareurl.php
+++ shareurl.php
@@ -5,7 +5,9 @@
 		$url = urlencode($url);
 		$title = '我正在用成绩查询代理系统查询这学期的成绩(gpaquery.sinaapp.com)，这学期我的绩点达到了'.$gpa.'，崇拜我吧！哈哈哈哈哈！';
 		$title = urlencode(mb_convert_encoding($title,'gb2312','utf-8' ));
-	 	$share_url_weibo = 'http://service.weibo.com/share/share.php?url='.$url.'&type=icon&language=zh_cn&appkey=5gBsE5&title='.$title.'&searchPic=true&style=simple';		
+		$pic = 'http://gpaquery.sinaapp.com/images/logo.png';
+		$pic = urlencode($pic);
+	 	$share_url_weibo = 'http://service.weibo.com/share/share.php?url='.$url.'&pic='.$pic.'&type=icon&language=zh_cn&appkey=5gBsE5&title='.$title.'&searchPic=true&style=simple';		
 		return $share_url_weibo;
 	}
 	function shareUrlRenren($gpa)
@@ -14,7 +16,9 @@
 		$url = urlencode($url);
 		$description = urlencode('我正在用成绩查询代理系统查询这学期的成绩(gpaquery.sinaapp.com)，这学期我的绩点达到了'.$gpa.'，崇拜我吧！哈哈哈哈哈！');
 		$title = urlencode('成绩查询代理系统-河海大学');
-		$share_url_renren = 'http://widget.renren.com/dialog/share?resourceUrl='.$url."&title=".$title."&description=".$description;
+		$pic = 'http://gpaquery.sinaapp.com/images/logo.png';
+		$pic = urlencode($pic);
+		$share_url_renren = 'http://widget.renren.com/dialog/share?resourceUrl='.$url."&title=".$title."&description=".$description.'&pic='.$pic;
 		return $share_url_renren;
 	}
 	function shareUrlQzone($gpa)
@@ -22,8 +26,10 @@
 		$url = 'http://gpaquery.sinaapp.com';
 		$url = urlencode($url);
 		$title = urlencode('成绩查询代理系统-河海大学');
+		$pic = 'http://gpaquery.sinaapp.com/images/logo.png';
+		$pic = urlencode($pic);
 		$summary = urlencode('我正在用成绩查询代理系统查询这学期的成绩(gpaquery.sinaapp.com)，这学期我的绩点达到了'.$gpa.'，崇拜我吧！哈哈哈哈哈！');
-		$share_url_qzone = 'http://sns.qzone.qq.com/cgi-bin/qzshare/cgi_qzshare_onekey?url='.$url.'&title='.$title.'&summary='.$summary;
+		$share_url_qzone = 'http://sns.qzone.qq.com/cgi-bin/qzshare/cgi_qzshare_onekey?url='.$url.'&title='.$title.'&summary='.$summary.'&pics='.$pic;
 		
 		return $share_url_qzone;
 	}
